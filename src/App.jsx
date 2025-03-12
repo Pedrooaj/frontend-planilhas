6749ae95a42b51253ed389414d150e478bc2f86f
pedrooaj <pedroantonio5735@gmail.com>
2025-03-12 11:17:07 -0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,25 @@
-// @ts-check
+import "./App.css";
+import Html5QrcodePlugin from "./services/Html5QrcodePlugin";
+import { useState } from "react";
 
-import React, { useState } from 'react';
-import './App.css';
-import HowToUse from './HowToUse.jsx';
-import Html5QrcodePlugin from './Html5QrcodePlugin.jsx';
-import ResultContainerPlugin from './ResultContainerPlugin.jsx';
+function App() {
+  const [decodedResults, setDecodedResults] = useState([]);
+  const onNewScanResult = (decodedText, decodedResult) => {
+    console.log("App [result]", decodedResult);
+    setDecodedResults((prev) => [...prev, decodedResult]);
+  };
 
-const App = (props) => {
-    const [decodedResults, setDecodedResults] = useState([]);
-    const onNewScanResult = (decodedText, decodedResult) => {
-        console.log("App [result]", decodedResult);
-        setDecodedResults(prev => [...prev, decodedResult]);
-    };
-
-    return (
-        <div className="App">
-            <section className="App-section">
-                <div className="App-section-title"> Html5-qrcode React demo</div>
-                <br />
-                <br />
-                <br />
-                <Html5QrcodePlugin
-                    fps={10}
-                    qrbox={250}
-                    disableFlip={false}
-                    qrCodeSuccessCallback={onNewScanResult}
-                />
-                <ResultContainerPlugin results={decodedResults} />
-                <HowToUse />
-            </section>
-        </div>
-    );
-};
+  return (
+    <>
+     <div className="App-section-title"> Html5-qrcode React demo</div>
+      <Html5QrcodePlugin
+        fps={10}
+        qrbox={250}
+        disableFlip={false}
+        qrCodeSuccessCallback={onNewScanResult}
+      />
+    </>
+  );
+}
 
 export default App;
--- src/services/Html5QrcodePlugin.jsx
+++ src/services/Html5QrcodePlugin.jsx
@@ -1,9 +1,8 @@
-import { Html5QrcodeScanner } from 'html5-qrcode';
-import { useEffect } from 'react';
+import { Html5QrcodeScanner } from "html5-qrcode";
+import { useEffect } from "react";
 
 const qrcodeRegionId = "html5qr-code-full-region";
 
-// Creates the configuration object for Html5QrcodeScanner.
 const createConfig = (props) => {
     let config = {};
     if (props.fps) {
@@ -22,29 +21,25 @@ const createConfig = (props) => {
 };
 
 const Html5QrcodePlugin = (props) => {
-
     useEffect(() => {
-        // when component mounts
         const config = createConfig(props);
         const verbose = props.verbose === true;
-        // Suceess callback is required.
-        if (!(props.qrCodeSuccessCallback)) {
-            throw "qrCodeSuccessCallback is required callback.";
+        if(!(props.qrCodeSuccessCallback)){
+            throw "qrCodeSuccessCallback e requerida.";
         }
         const html5QrcodeScanner = new Html5QrcodeScanner(qrcodeRegionId, config, verbose);
         html5QrcodeScanner.render(props.qrCodeSuccessCallback, props.qrCodeErrorCallback);
 
-        // cleanup function when component will unmount
         return () => {
             html5QrcodeScanner.clear().catch(error => {
-                console.error("Failed to clear html5QrcodeScanner. ", error);
+                console.error("Falha ao realizar limpeza do html5QrcodeScanner ", error);
             });
         };
-    }, []);
+    }, [])
 
     return (
         <div id={qrcodeRegionId} />
-    );
-};
+    )
+}
 
 export default Html5QrcodePlugin;
