778a31ad5b674e5e5003cc09ac670e81584f0dfa
pedrooaj <pedroantonio5735@gmail.com>
2025-03-12 16:56:56 -0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -4,64 +4,45 @@ import { Html5Qrcode } from "html5-qrcode";
 import styled from "styled-components";
 
 const Container = styled.div`
-  text-align: center;
+
 `;
 
 function App() {
   const [barcode, setBarcode] = useState("");
-  const [scannerRunning, setScannerRunning] = useState(false); // Novo estado para monitorar o scanner
 
   useEffect(() => {
     const scanner = new Html5Qrcode("reader");
 
     const onScanSuccess = (decodedText, decodedResult) => {
       setBarcode(decodedText);
-      if (scannerRunning) {
-        scanner.stop().then(() => {
-          setScannerRunning(false); // Atualiza o estado quando o scanner é parado
-          console.log("Scanner stopped successfully");
-        }).catch((err) => {
-          console.error("Error stopping scanner: ", err);
-        });
-      }
+      scanner.stop().then(() => {
+        console.log("Scanner parado com sucesso");
+      }).catch((err) => {
+        console.error("Erro ao parar Scanner: ", err);
+      });
     };
 
     const onScanFailure = (error) => {
-      console.warn(`Code scan error: ${error}`);
+      console.log(`Erro ao realizar Scan: ${error}`);
     };
 
-    // Inicia o scanner e atualiza o estado para "running"
     scanner.start(
-      { facingMode: "environment" },
+      { facingMode: "environment" }, // Corrigido para passar a string 'environment'
       {
-        fps: 10,
-        qrbox: { height: 250, width: 250 },
+        fps: 10, // Frames per second
+        qrbox: { height: 250, width: 250 }, // QR code scanning box size
         disableFlip: false,
       },
       onScanSuccess,
       onScanFailure
-    ).then(() => {
-      setScannerRunning(true); // Marca como "running" quando o scanner começar
-    }).catch((err) => {
-      console.error("Error starting scanner: ", err);
-    });
+    );
 
-    // Cleanup function to stop the scanner when the component unmounts
-    return () => {
-      if (scannerRunning) {
-        scanner.stop().then(() => {
-          console.log("Scanner stopped on unmount");
-        }).catch((err) => {
-          console.error("Error stopping scanner on unmount: ", err);
-        });
-      }
-    };
-  }, [scannerRunning]); // Adiciona scannerRunning ao array de dependências para garantir que o stop() seja chamado corretamente
+  }, []);
 
   return (
     <Container>
-      <div id="reader" style={{ width: "100%" }}></div>
-      <h1>{barcode ? `Scanned Barcode: ${barcode}` : "Scan a QR code"}</h1>
+      <div id="reader" ></div>
+      <h1>{barcode ? `Código de barras detectado: ${barcode}` : "Scaneie o codigo de barras"}</h1>
     </Container>
   );
 }
