955f1b7592678959fa0ea9be72770e8012042821
pedrooaj <pedroantonio5735@gmail.com>
2025-03-12 09:59:23 -0300

    update

--- src/Approutes.jsx
+++ src/Approutes.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./routes/Home";
+import Home from "./routes/Home.jsx";
 import { GlobalProvider } from "./Contexts/GlobalContext";
-import Scanner from "./Routes/Scanner";
+import Scanner from "./Routes/Scanner.jsx";
 
 
 const AppRoutes = () => {
