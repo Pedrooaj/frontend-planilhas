d5a49e4f104333282d79d268185e5f59ec044d80
pedrooaj <pedroantonio5735@gmail.com>
2025-03-12 09:52:52 -0300

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import AppRoutes from './AppRoute.jsx'
+import AppRoutes from './Approutes.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
