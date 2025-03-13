079e8e8fe3c66e26822f4d9ab076aaf019268263
Pedrooaj <pedroantonio5735@gmail.com>
2025-03-13 18:02:38 -0300

    update

--- src/context/Globalcontext.jsx
+++ src/context/Globalcontext.jsx
@@ -21,14 +21,14 @@ const GlobalProvider = ({ children }) => {
   };
   
   const adicionarPatrimonio = (patrimonio) => {
-    if (patrimonio.length > 8) {
-      toast.error("Patrimônio Inválido",{ autoClose: 3000, position: "bottom-center" });
-    }else if(!["60", "87", "83", "087"].includes(patrimonio.slice(0,2))){
-      toast.error("Patrimônio Inválido",{ autoClose: 3000, position: "bottom-center" });
-    }else{
-      if(["87","83","087"].includes(patrimonio.slice(0,2))){
-        patrimonio = patrimonio.slice(3 )
-      }
+    // if (patrimonio.length > 8) {
+    //   toast.error("Patrimônio Inválido",{ autoClose: 3000, position: "bottom-center" });
+    // }else if(!["60", "87", "83", "087"].includes(patrimonio.slice(0,2))){
+    //   toast.error("Patrimônio Inválido",{ autoClose: 3000, position: "bottom-center" });
+    // }else{
+    //   if(["87","83","087"].includes(patrimonio.slice(0,2))){
+    //     patrimonio = patrimonio.slice(3)
+    //   }
         if (!lista.patrimonios.includes(patrimonio)) {
             setLista((prevLista) => ({
               ...prevLista,
@@ -44,7 +44,7 @@ const GlobalProvider = ({ children }) => {
               position: "bottom-center",
             });
           }
-    }
+    // }
 
    
   };
