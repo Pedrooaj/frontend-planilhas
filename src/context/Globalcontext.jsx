45ed6719a5e45d2485666f9a670a216c4559da9a
Pedrooaj <pedroantonio5735@gmail.com>
2025-03-13 18:36:06 -0300

    update

--- src/context/Globalcontext.jsx
+++ src/context/Globalcontext.jsx
@@ -22,16 +22,16 @@ const GlobalProvider = ({ children }) => {
 
   const adicionarPatrimonio = (patrimonio) => {
     if (patrimonio.length > 8) {
-      toast.error("Patrimônio Inválido", { autoClose: 3000, position: "bottom-center" });
+      toast.error("Patrimônio Inválido 1", { autoClose: 3000, position: "bottom-center" });
 
     } else if (!["60", "87", "83"].includes(patrimonio.slice(0, 2)) || !["087"].includes(patrimonio.slice(0,3))) {
-      toast.error("Patrimônio Inválido ", { autoClose: 3000, position: "bottom-center" });
+      toast.error("Patrimônio Inválido 2", { autoClose: 3000, position: "bottom-center" });
     }
 
-    if(["87", "83"].includes(patrimonio.slice(2))){
-      patrimonio = patrimonio.slice(0,2);
-    }else if("087".includes(patrimonio.slice(3))){
-      patrimonio = patrimonio.slice(0,3);
+    if(["87", "83"].includes(patrimonio.slice(0, 2))){
+      patrimonio = patrimonio.slice(2);
+    }else if("087".includes(patrimonio.slice(0,3))){
+      patrimonio = patrimonio.slice(3);
     }
 
     if (!lista.patrimonios.includes(patrimonio)) {
