ef375a3c3b12b50dec8c2a0a0fafb90e179ab1fa
pedrooaj <pedroantonio5735@gmail.com>
2025-03-27 14:06:40 -0300

    update

--- src/context/Globalcontext.jsx
+++ src/context/Globalcontext.jsx
@@ -20,43 +20,48 @@ const GlobalProvider = ({ children }) => {
     }));
   };
 
+  
   const adicionarPatrimonio = (patrimonio) => {
+    // Verifica se o patrimônio tem no máximo 8 caracteres
     if (patrimonio.length > 8) {
       toast.error("Patrimônio deve ter no máximo 8 caracteres", { autoClose: 3000, position: "bottom-center" });
-    } else if (!["60", "87", "83", "087"].some((prefixo) => patrimonio.startsWith(prefixo))) {
-      // verifica se o prefixo e válido
-      toast.error(<span>Patrimônio <b>{patrimonio}</b> Inválido </span>, { autoClose: 3000, position: "bottom-center" });
+      return;
     }
-
-
-    // formata os patrimônios antes de adicionar a lista
-    if(["87", "83"].includes(patrimonio.slice(0, 2))){
+  
+    // Verifica se o prefixo é válido
+    const prefixosValidos = ["60", "87", "83", "087"];
+    if (!prefixosValidos.some((prefixo) => patrimonio.startsWith(prefixo))) {
+      toast.error(<span>Patrimônio <b>{patrimonio}</b> Inválido</span>, { autoClose: 3000, position: "bottom-center" });
+      return;
+    }
+  
+    // Formata os patrimônios antes de adicionar à lista
+    if (["87", "83"].includes(patrimonio.slice(0, 2))) {
       patrimonio = patrimonio.slice(2);
-    }else if("087" === (patrimonio.slice(0,3))){
+    } else if (patrimonio.startsWith("087")) {
       patrimonio = patrimonio.slice(3);
     }
-
-
-    // verifica se o patrimônio ja está na lista
-    if (!lista.patrimonios.includes(patrimonio)) {
-      setLista((prevLista) => ({
-        ...prevLista,
-        patrimonios: [...prevLista.patrimonios, patrimonio],
-      }));
-      toast.success(<span>Patrimônio Adicionado: <b>{patrimonio}</b></span>, {
-        autoClose: 3000,
-        position: "bottom-center",
-      });
-    } else {
-      toast.warn("Patrimônio ja está na lista...", {
-        autoClose: 3000,
-        position: "bottom-center",
-      });
+  
+    // Verifica se o patrimônio já está na lista
+    if (lista.patrimonios.includes(patrimonio)) {
+      toast.warn("Patrimônio já está na lista...", { autoClose: 3000, position: "bottom-center" });
+      return;
     }
-
-
-
+  
+    // Adiciona à lista
+    setLista((prevLista) => ({
+      ...prevLista,
+      patrimonios: [...prevLista.patrimonios, patrimonio],
+    }));
+  
+    toast.success(<span>Patrimônio Adicionado: <b>{patrimonio}</b></span>, {
+      autoClose: 3000,
+      position: "bottom-center",
+    });
   };
+  
+
+
 
   const adicionarDestino = (destino) => {
     setLista((prevLista) => ({
